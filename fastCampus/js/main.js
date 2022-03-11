817c2cf412b5d07c19b1c4dbff0259c24a620f0f
Woogie95 <choisw950928@naver.com>
2022-03-11 17:11:07 +0900

    FastCampus_Final

--- fastCampus/js/main.js
+++ fastCampus/js/main.js
@@ -1,10 +1,26 @@
 // 메뉴 이동 
 function setMenu(_menu) {
- // setMenu 함수를 완성해주세요.
+  var menus = document.querySelectorAll("nav li");
+  menus.forEach(function (menu) {
+    menu.classList.remove('on');
+  });
+  document.querySelector("nav li." + _menu).classList.add("on");
+  document.querySelector("main").className = _menu;
 }
 
 function showVideoClips() {
-  // showVideoClips 함수를 완성해주세요.
+  var videos = document.querySelector('.video-list');
+  videos.forEach(function (video) {
+    var videoNode = document.querySelector(".video-item[hidden]").cloneNode(true);
+    videoNode.querySelector(".thumbnail").src = './img/' + video.thumbnail_src;
+    videoNode.querySelector(".profile").src = './img/' + video.profile_src;
+    videoNode.querySelector(".title").innerHTML = video.title;
+    videoNode.querySelector(".author").innerHTML = video.author;
+    videoNode.querySelector(".stat").innerHTML = video.stat;
+    videoNode.hidden = false;
+    videoList.appendChild(videoNode);
+  })
+
 }
 
 function init() {
